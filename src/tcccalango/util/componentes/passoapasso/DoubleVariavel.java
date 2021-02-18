6c7adc523fbd3cceaba04e6db8e14acbf993e7c3
GeovanaRamos <geovannna.1998@gmail.com>
2021-02-18 17:36:44 -0300

    Correcting number formatting on debugger

--- src/tcccalango/util/componentes/passoapasso/DoubleVariavel.java
+++ src/tcccalango/util/componentes/passoapasso/DoubleVariavel.java
@@ -1,11 +1,15 @@
 package tcccalango.util.componentes.passoapasso;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class DoubleVariavel extends DefaultVariavel {
-   private static final DecimalFormat df = new DecimalFormat("0.000");
+   private final static NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
+   private static final DecimalFormat df = (DecimalFormat)nf;
 
    public final void setValor(Object valor) {
+      df.setGroupingUsed(false);
       if (valor != null && valor instanceof Number) {
          valor = df.format((Number)valor);
       }
