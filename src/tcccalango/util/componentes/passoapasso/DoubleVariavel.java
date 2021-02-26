330fd6c63f5767d19b896593b3001226570ec4d1
GeovanaRamos <geovannna.1998@gmail.com>
2021-02-26 15:25:20 -0300

    Showing debugger variable decimals

--- src/tcccalango/util/componentes/passoapasso/DoubleVariavel.java
+++ src/tcccalango/util/componentes/passoapasso/DoubleVariavel.java
@@ -1,17 +1,12 @@
 package tcccalango.util.componentes.passoapasso;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Locale;
 
 public class DoubleVariavel extends DefaultVariavel {
-   private final static NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
-   private static final DecimalFormat df = (DecimalFormat)nf;
 
    public final void setValor(Object valor) {
-      df.setGroupingUsed(false);
-      if (valor != null && valor instanceof Number) {
-         valor = df.format((Number)valor);
+      if (valor instanceof Number) {
+         valor = String.format(Locale.US, "%s", valor);
       }
 
       super.setValor(valor);
